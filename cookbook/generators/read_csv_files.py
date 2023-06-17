5a5a79dc2a59102ef395836e727eb1e7ed74a3a8
pkgone <praneethchandra@gmail.com>
2023-06-17 08:09:47 -0700

    add profiling to return and yield

--- cookbook/generators/read_csv_files.py
+++ cookbook/generators/read_csv_files.py
@@ -1,3 +1,5 @@
+import cProfile
+import timeit
 
 def csv_reader_using_return(file_name):
     file = open(file_name, "r")
@@ -9,7 +11,7 @@ def csv_reader_using_yield(file_name):
         yield row
 
 def read_using_return():
-    csv_gen = csv_reader_using_return("/home/pgone/py/py_exercises/files/gender_statistics.csv")
+    csv_gen = csv_reader_using_return("/home/pkgone/py_exercises/files/gender_statistics.csv")
     row_count = 0
 
     for row in csv_gen:
@@ -18,13 +20,23 @@ def read_using_return():
     print(f"Row count is {row_count}")
 
 def read_using_yield():
-    csv_gen = csv_reader_using_yield("/home/pgone/py/py_exercises/files/gender_statistics.csv")
+    csv_gen = csv_reader_using_yield("/home/pkgone/py_exercises/files/gender_statistics.csv")
     row_count = 0
 
     for row in csv_gen:
         row_count += 1
 
     print(f"Row count is {row_count}")
+
+
+def main():
+    timeit.timeit("read_using_return()", number=1, setup="from __main__ import read_using_return")
+    timeit.timeit("read_using_yield()", number=1, setup="from __main__ import read_using_yield")
+
+
+if __name__ == '__main__':
+    cProfile.run('main()')
+    # main()
 
 
 
